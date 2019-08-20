aa3750b527a0cb801e36f779444bf47825672407
Slavi <slaveizlatarev@gmail.com>
2019-08-21 02:47:45 +0300

    Format code .

--- Functional Programming - Exercises/src/ReverseAndExclude.java
+++ Functional Programming - Exercises/src/ReverseAndExclude.java
@@ -12,17 +12,22 @@ public class ReverseAndExclude {
     public static void main(String[] args) throws IOException {
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
 
-        List<Integer> nums = Arrays.stream(read.readLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
+        List<Integer> nums = Arrays.stream(read.readLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
         int divisibleNum = Integer.parseInt(read.readLine());
 
-        Predicate<Integer> filterByCriteria = num -> num%divisibleNum!=0;
+        Predicate<Integer> filterByCriteria = num -> num % divisibleNum != 0;
 
-        UnaryOperator<List<Integer>> removeDivisibleByNum = list -> list.stream().filter(filterByCriteria).collect(Collectors.toList());
+        UnaryOperator<List<Integer>> removeDivisibleByNum = list -> list.stream()
+                .filter(filterByCriteria)
+                .collect(Collectors.toList());
 
         nums = removeDivisibleByNum.apply(nums);
 
         Collections.reverse(nums);
 
-        System.out.println(nums.toString().replaceAll("[\\[\\],]",""));
+        System.out.println(nums.toString().replaceAll("[\\[\\],]", ""));
     }
 }
