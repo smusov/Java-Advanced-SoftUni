b003d2b51712f812fbc9700055e51d28c12c5031
Slavi <slaveizlatarev@gmail.com>
2019-09-07 00:27:59 +0300

    Add task : CustomStack .

---
+++ Workshop-Custom Data Structures/src/SmartArray/CustomStack.java
@@ -0,0 +1,53 @@
+package SmartArray;
+
+import java.util.NoSuchElementException;
+import java.util.function.Consumer;
+
+public class CustomStack {
+    private static final int INITIAL_CAPACITY = 4;
+    private int[] items;
+    private int size;
+    private int capacity;
+
+    public CustomStack() {
+        this.items = new int[INITIAL_CAPACITY];
+        this.capacity = INITIAL_CAPACITY;
+    }
+
+    public void push (int element){
+
+        if (this.size == capacity){
+            resize();
+        }
+
+        this.items[size++] = element;
+    }
+    public int pop(){
+        isEmpty();
+        return this.items[--this.size];
+    }
+
+    public Consumer<Integer> foreach (Consumer <Integer>consumer){
+        for (int i = 0; i < this.size; i++) {
+            consumer.accept(items[i]);
+        }
+        return consumer;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    private void resize() {
+        this.capacity*=2;
+        int[] resizedArray = new int[this.capacity];
+        if (this.size >= 0) System.arraycopy(items, 0, resizedArray, 0, this.size);
+        this.items = resizedArray;
+    }
+    private void isEmpty (){
+        if (this.size==0){
+            throw new NoSuchElementException("CustomStack is empty");
+        }
+    }
+
+}
