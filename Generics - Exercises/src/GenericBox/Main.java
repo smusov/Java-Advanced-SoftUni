cde80e2135d8dc3ef987030424304f98673462b2
Slavi <slaveizlatarev@gmail.com>
2019-09-11 12:47:56 +0300

    Add task : Generic Box .

---
+++ Generics - Exercises/src/GenericBox/Main.java
@@ -0,0 +1,19 @@
+package GenericBox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(read.readLine());
+
+        for (int i = 0; i < n; i++) {
+            GenericBox <String> genericBox = new GenericBox<>(read.readLine());
+            System.out.println(genericBox.toString());
+        }
+
+    }
+}
