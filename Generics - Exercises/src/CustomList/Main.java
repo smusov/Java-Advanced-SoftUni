44e19a1a174a63f6d43fecafc47a7e7ecd33928a
Slavi <slaveizlatarev@gmail.com>
2019-09-12 23:08:35 +0300

    Add task : Custom List .

---
+++ Generics - Exercises/src/CustomList/Main.java
@@ -0,0 +1,49 @@
+package CustomList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+
+        CustomList<String> list = new CustomList<String>();
+
+        String input;
+        
+        while(!"END".equals(input=read.readLine())){
+
+            String[] data = input.split("\\s+");
+
+            switch (data[0]){
+                case "Add":
+                    list.add(data[1]);
+                    break;
+                case "Remove":
+                    list.remove(Integer.parseInt(data[1]));
+                    break;
+                case "Contains":
+                    System.out.println(list.contains(data[1]));
+                    break;
+                case "Swap":
+                    list.swap(Integer.parseInt(data[1]),Integer.parseInt(data[2]));
+                    break;
+                case "Greater":
+                    System.out.println(list.countGreaterThan(data[1]));
+                    break;
+                case "Max":
+                    System.out.println(list.getMax());
+                    break;
+                case "Min":
+                    System.out.println(list.getMin());
+                    break;
+                case "Print":
+                    for (int i = 0; i < list.size(); i++) {
+                        System.out.println(list.get(i));
+                    }
+                    break;
+            }
+        }
+    }
+}
