cfcdd32f5fb9821079592ad46a7d2011b50f636d
Slavi <slaveizlatarev@gmail.com>
2019-09-12 23:50:30 +0300

    Add task : Custom List Iterator .

---
+++ Generics - Exercises/src/CustomListIterator/Main.java
@@ -0,0 +1,52 @@
+package CustomListIterator;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+
+        CustomList<String> list = new CustomList<>();
+
+        String input;
+        
+        while(!"END".equals(input=read.readLine())){
+
+            String[] data = input.split("\\s+");
+
+            switch (data[0]){
+                case "Add":
+                    list.add(data[1]);
+                    break;
+                case "Remove":
+                    list.remove(Integer.parseInt(data[1]));
+                    break;
+                case "Contains":
+                    System.out.println(list.contains(data[1]));
+                    break;
+                case "Swap":
+                    list.swap(Integer.parseInt(data[1]),Integer.parseInt(data[2]));
+                    break;
+                case "Greater":
+                    System.out.println(list.countGreaterThan(data[1]));
+                    break;
+                case "Max":
+                    System.out.println(list.getMax());
+                    break;
+                case "Min":
+                    System.out.println(list.getMin());
+                    break;
+                case "Print":
+                    for (String element : list) {
+                        System.out.println(element);
+                    }
+                    break;
+                case "Sort":
+                    Sorter.sort(list);
+                    break;
+            }
+        }
+    }
+}
