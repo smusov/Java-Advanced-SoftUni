4042c4e170f70bd10054ff37072c05158594fa0a
Slavi <slaveizlatarev@gmail.com>
2019-09-13 01:24:08 +0300

    Add task : Threeuple .

---
+++ Generics - Exercises/src/Threeuple/Main.java
@@ -0,0 +1,35 @@
+package Threeuple;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] data = read.readLine().split("\\s+");
+
+        Threeuple<String,String,String> nameAddressTown = new Threeuple<>(data[0] + " " + data[1], data[2], data[3]);
+
+        System.out.println(nameAddressTown);
+
+        data = read.readLine().split("\\s+");
+
+        Threeuple<String,Integer,Boolean> nameLiterBeerDrunk;
+
+        if (data[2].equals("drunk")){
+            nameLiterBeerDrunk = new Threeuple<>(data[0], Integer.parseInt(data[1]), true);
+        }else {
+            nameLiterBeerDrunk = new Threeuple<>(data[0], Integer.parseInt(data[1]), false);
+        }
+
+        System.out.println(nameLiterBeerDrunk);
+
+        data = read.readLine().split("\\s+");
+
+        Threeuple<String,Double,String> nameBalanceBank = new Threeuple<>(data[0], Double.parseDouble(data[1]), data[2]);
+
+        System.out.println(nameBalanceBank);
+    }
+}
---
+++ Generics - Exercises/src/Threeuple/Threeuple.java
@@ -0,0 +1,18 @@
+package Threeuple;
+
+public class Threeuple<item1,item2,item3> {
+    private item1 item1;
+    private item2 item2;
+    private item3 item3;
+
+    public Threeuple(item1 item1, item2 item2, item3 item3) {
+        this.item1 = item1;
+        this.item2 = item2;
+        this.item3 = item3;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s -> %s -> %s",this.item1,this.item2,this.item3);
+    }
+}
