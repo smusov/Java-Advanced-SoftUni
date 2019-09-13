6fc3ac748d52eeadc0c4e08670f96c4f2c3982a4
Slavi <slaveizlatarev@gmail.com>
2019-09-13 23:53:22 +0300

    Add task : ListyIterator .

---
+++ Iterators and Comparators - Exercises/src/ListyIterator/Main.java
@@ -0,0 +1,36 @@
+package ListyIterator;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+
+        List<String> data = Arrays.stream(read.readLine().split("\\s+"))
+                .filter(e->!e.equals("Create")).collect(Collectors.toList());
+
+        ListyIterator listyIterator = new ListyIterator(data);
+
+        String input;
+
+        while(!"END".equals(input=read.readLine())){
+
+            switch (input){
+                case "Move":
+                    System.out.println(listyIterator.move());
+                    break;
+                case "Print":
+                    listyIterator.print();
+                    break;
+                case "HasNext":
+                    System.out.println(listyIterator.hasNext());
+                    break;
+            }
+        }
+    }
+}
