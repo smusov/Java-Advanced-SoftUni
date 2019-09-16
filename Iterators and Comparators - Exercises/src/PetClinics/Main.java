7db35fd228ff76ff0600d98e39eb8b1e55b1d463
Slavi <slaveizlatarev@gmail.com>
2019-09-16 23:19:35 +0300

    Add task : Pet Clinics .

---
+++ Iterators and Comparators - Exercises/src/PetClinics/Main.java
@@ -0,0 +1,66 @@
+package PetClinics;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+
+        Map<String, Clinic> shelters = new HashMap<>();
+        Map<String, Pet> pets = new HashMap<>();
+
+        int n = Integer.parseInt(sc.readLine());
+
+        for (int i = 0; i < n; i++) {
+
+            String[] data = sc.readLine().split("\\s+");
+
+            switch (data[0]) {
+                case "Create":
+
+                    if (data[1].equals("Pet")) {
+
+                        Pet pet = new Pet(data[2], Integer.parseInt(data[3]), data[4]);
+                        pets.putIfAbsent(data[2], pet);
+
+                    } else {
+
+                        try {
+                            Clinic clinic = new Clinic(data[2], Integer.parseInt(data[3]));
+                            shelters.putIfAbsent(data[2], clinic);
+                        } catch (IllegalArgumentException e) {
+                            System.out.println(e.getMessage());
+                        }
+
+                    }
+
+                    break;
+                case "Add":
+                    System.out.println(shelters.get(data[2]).add(pets.get(data[1])));
+                    break;
+                case "Release":
+                    System.out.println(shelters.get(data[1]).release());
+                    break;
+                case "HasEmptyRooms":
+                    System.out.println(shelters.get(data[1]).HasEmptyRooms());
+                    break;
+                case "Print":
+
+                    if (data.length == 2) {
+                        shelters.get(data[1]).forEach(pet -> {
+                            if (pet != null){
+                                System.out.println(pet);
+                            }else{
+                                System.out.println("Room empty");
+                            }
+                        });
+
+                    } else {
+                        System.out.println(shelters.get(data[1]).printRoom(Integer.parseInt(data[2]) - 1));
+                    }
+                    break;
+            }
+        }
+    }
+}
