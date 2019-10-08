003aaa627115b0723a131e2eb29bb47bb9a674dc
Slavi <slaveizlatarev@gmail.com>
2019-10-09 00:08:47 +0300

    Converted all tasks conditions in pdf , for easy read direct in github .

---
+++ Workshop-Basic Algorithms/src/QuickSort.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class QuickSort {
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+
+        //TODO : За решаване!
+
+    }
+}
+
